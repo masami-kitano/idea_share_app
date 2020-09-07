64b17834197f3ccd56868f6f830e5d27a3098944
masami.kitano <techacademy.example@gmal.com>
2020-09-07 21:47:57 +0900

    add user image

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
-
+  
+  has_many :posts, dependent: :restrict_with_error
+  
   validates :name, presence: true, length: { maximum: 255}
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_121446) do
+ActiveRecord::Schema.define(version: 2020_09_07_122854) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_121446) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   add_foreign_key "categories", "users"
