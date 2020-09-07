461450b98ba5a748c5b46345d0b2c2c9b51ecd6b
masami.kitano <techacademy.example@gmal.com>
2020-09-07 23:34:50 +0900

    modify miss

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,12 +1,15 @@
 class ToppagesController < ApplicationController
   def index
-    @users = User.all
+    if logged_in?
+      @users = User.all
       
-    @category = Category.new
-    @categories = Category.all
-
-    @post = current_user.posts.build
-
-    @comment = current_user.comments.build
+      @category = Category.new
+      @categories = Category.all
+      
+      @post = current_user.posts.build
+      
+      @comment = current_user.comments.build
+     
+    end
   end
 end
