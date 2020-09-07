e6e46c262561f12113f1469742fe53d37e9abc96
masami.kitano <techacademy.example@gmal.com>
2020-09-07 19:23:35 +0900

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    # @categories = Category.all
+    @user = User.find(params[:id])
+    # @posts = @user.posts.order(id: :desc).page(params[:page])
+    # counts(@user)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
+end
